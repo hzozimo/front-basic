d0443914371b28d1d8549de97fa9e42e07fb1ec9
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2021-09-23 18:59:19 -0300

    alteraçoes de codigo para melhorias

--- scripts.js
+++ scripts.js
@@ -42,7 +42,6 @@ const createLinkToInstagram = (photo, index) => {
   hoverInfo[index].setAttribute('target', '_blank');
 }
 
-
 document.addEventListener("DOMContentLoaded", async function () {
   // Código que será executado quando o navegador carregar
   const URL = "https://us-central1-squid-apis.cloudfunctions.net/test-front-basic";
