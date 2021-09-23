ab3f03f19aa5c46157b410b5cf5fa32d30dc6386
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2021-09-23 13:27:03 -0300

    imagens pegas da api, colocadas na pagina, criadas as div para imagens, hover e textos com overlay

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", async function () {
   // Código que será executado quando o navegador carregar
+  const URL = "https://us-central1-squid-apis.cloudfunctions.net/test-front-basic";
+  const apiData = await (await fetch(URL)).json();
+  console.log(apiData);
+  const allImages = document.getElementById('all-images');
+  apiData.map((photo, index)=>{
+    const createContainerDiv = document.createElement('div');
+    const imageDiv = document.createElement('div');
+    const hoverDiv = document.createElement('div');
+    const textDiv = document.createElement('div');
+    allImages.appendChild(createContainerDiv).classList.add('container');
+    const container = document.getElementsByClassName('container');
+    container[index].appendChild(imageDiv).classList.add('instagram-photo');
+    container[index].appendChild(hoverDiv).classList.add('overlay');
+    const instagramPhoto = document.getElementsByClassName('instagram-photo');
+    const createImg = document.createElement('img');
+    createImg.src = photo.imagens.resolucaoPadrao.url;
+    instagramPhoto[index].appendChild(createImg).classList.add('image');
+    const hoverInfo = document.getElementsByClassName('overlay');
+    hoverInfo[index].appendChild(textDiv).classList.add('text');
+    const text = document.getElementsByClassName('text');
+    text[index].innerText = "hello world"
+  })
 });
